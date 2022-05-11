e616eee192841baad54bdc55a61b5c457af93af1
Alexander Yakovlev <coldwire45@gmail.com>
2022-05-11 15:49:01 +0300

    Update study file

---
+++ my-program.py
@@ -0,0 +1,14 @@
+#def def_temp(temp):
+#    if temp > 7:
+#        return "Warm"
+#    else:
+#        return "Cold"
+
+#user_input = int(input("Enter temperature: "))
+
+#output = def_temp(user_input)
+
+user_input = input("Enter your name: ")
+message = "Hello %s!" % user_input
+message = f"Hello {user_input}"
+print(message)
